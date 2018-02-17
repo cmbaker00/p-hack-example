026780c2383a04b0ae85be05335610c71443d289
cmbaker00 <cmbaker00@gmail.com>
2018-02-17 19:30:19 +1100

    added code to test number of questions required

--- phack.py
+++ phack.py
@@ -1,6 +1,7 @@
 # import scipy
 from scipy.stats import ttest_ind as ttest
 import requests
+import numpy as np
 
 response = requests.get('https://docs.google.com/spreadsheet/ccc?key=1BP4mcPosl2V5U_Q9HwJ9VriwdSS4LaSMXILXsEOPKfA&output=csv')
 rd = response.content.decode()
@@ -16,5 +17,22 @@ def deal_with_data(resp):
         if nspt > lb:
             flag = 1
             break
+
+def test_ttest(nrep,nqs):
+    pvec = []
+    g1 = np.random.random([nrep,nqs])
+    g2 = np.random.random([nrep,nqs])
+    for i in range(nqs):
+        t,p = ttest(g1[:,i],g2[:,i])
+        pvec.append(p)
+    return pvec
+
+def rep_test_ttest(nrep,nqs,reps):
+    pval_vec = []
+    for i in range(reps):
+        p = test_ttest(nrep, nqs)
+        pval_vec.append(np.min(p))
+    pval_vec = np.array(pval_vec)
+    return np.mean(pval_vec < .05)
 
 deal_with_data(rd)
