0262d4e0da08c91b4da04e64db27bd450d2cab8a
cmbaker00 <cmbaker00@gmail.com>
2018-02-17 15:55:49 +1100

    grab data from google docs

--- phack.py
+++ phack.py
@@ -1,2 +1,20 @@
 # import scipy
 from scipy.stats import ttest_ind as ttest
+import requests
+
+response = requests.get('https://docs.google.com/spreadsheet/ccc?key=1BP4mcPosl2V5U_Q9HwJ9VriwdSS4LaSMXILXsEOPKfA&output=csv')
+rd = response.content.decode()
+
+def deal_with_data(resp):
+    lb = resp.find('\n')
+    flag = 0
+    csp = 0
+    while flag == 0:
+        nspt = resp.find(',',csp)
+        print(resp[csp:nspt])
+        csp = nspt + 1
+        if nspt > lb:
+            flag = 1
+            break
+
+deal_with_data(rd)
