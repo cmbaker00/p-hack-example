91dc27dfd268b562071cd755931a297f1431a2b5
cmbaker00 <cmbaker00@gmail.com>
2018-02-20 14:36:22 +1000

    Altered the code to deal with only yes/no answers, and loop across all possible ways to divide the answers into two groups

--- phack.py
+++ phack.py
@@ -2,8 +2,9 @@
 from scipy.stats import ttest_ind as ttest
 import requests
 import numpy as np
+import warnings
 
-spreadsheet_id = '18ErdR3Vs3hKYYPQOoAxQ2YNwvACTJzK5iwx37UOQbOc'
+spreadsheet_id = '1oomfOjU8asC6aB-s-bffhvqzsA9QtdJn8RTU3anmLe0'
 
 response = requests.get('https://docs.google.com/spreadsheet/ccc?key=' + spreadsheet_id + '&output=csv')
 rd = response.content.decode()
@@ -62,20 +63,50 @@ def rep_test_ttest(nrep,nqs,reps):
 def np_array_flt_int(ar):
     return np.array([int(val) for val in ar])
 
+
+def ttest_single_split(data, split_index):
+    y_index = data[:,split_index] == 'Yes'
+    n_index = data[:,split_index] == 'No'
+    for i in range(data.shape[1]):
+        if i == split_index:
+            continue
+        y_data = data[y_index,i] == 'Yes'
+        n_data = data[n_index,i] == 'Yes'
+        tvalue, pvalue = ttest(y_data, n_data)
+
+
+        if pvalue < .05 and tvalue != -np.inf:
+            # print(data[0, split_index])
+            # print(data[0, i])
+            # print(np.mean(y_data))
+            # print(np.mean(n_data))
+            # print(pvalue)
+            print("""
+            If you like {0}, there is a {1}% chance that you like {2}
+            If you don't like {0}, there is a {3}% chance that you like {2}
+            Pvalue = {4}
+            """.format(data[0, split_index], np.round(100*np.mean(y_data),2), data[0, i], np.round(100*np.mean(n_data),2), np.round(pvalue,2)
+                  )
+                  )
+            1
 d = np.array(deal_with_data(rd))
+d = d[:,1:d.shape[1]]
 nrows = d.shape[0]
 ncols = d.shape[1]
 # print(d.shape)
 yrep = d[:,2] == 'Yes'
 nrep = d[:,2] == 'No'
-print(yrep)
-print(nrep)
-print(ncols)
-for i in range(3, ncols):
-    r1 = np_array_flt_int(d[yrep,i])
-    r2 = np_array_flt_int(d[nrep,i])
-    t, p = ttest(r1, r2)
-    print("""
-    {0}, y ave: {1}, n ave: {2}, pvalue: {3}""".format(d[0,i], np.mean(r1), np.mean(r2), p))
+# print(yrep)
+# print(nrep)
+# print(ncols)
+#
+# for i in range(3, ncols):
+#     r1 = np_array_flt_int(d[yrep,i])
+#     r2 = np_array_flt_int(d[nrep,i])
+#     t, p = ttest(r1, r2)
+#     print("""
+#     {0}, y ave: {1}, n ave: {2}, pvalue: {3}""".format(d[0,i], np.mean(r1), np.mean(r2), p))
 
 # print(rep_test_ttest(20, 30, 5000))
+for i in range(ncols):
+    ttest_single_split(d, i)
